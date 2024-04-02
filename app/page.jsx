a8ac25381609341a5f4d8b7f02fbd5b908caaba2
Hachem-SH <hachemcatalone@gmail.com>
2024-04-02 22:52:17 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.jsx
@@ -0,0 +1,89 @@
+"use client"
+import React, { useEffect } from 'react';
+import { useGameActions } from "@/hooks/useGameActions";
+
+
+const App = () => {
+  const { handlePlayClick } = useGameActions();
+  useEffect(() => {
+    // Empêcher le défilement lorsque l'application est en plein écran
+    document.documentElement.style.overflow = 'hidden';
+
+    // Remettre le défilement lorsque le composant est démonté
+    return () => {
+      document.documentElement.style.overflow = 'auto';
+    };
+  }, []);
+
+  const test = () => {
+    console.log('test');
+    handlePlayClick();
+  }
+
+
+  return (
+    <div
+      className="relative flex justify-center items-center bg-cover bg-center h-screen"
+      style={{
+        backgroundImage: `url('/main/fondliege.png')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'repeat',
+      }}
+    >
+      {/* Conteneur pour la page par-dessus */}
+      <div
+        className="relative"
+        style={{
+          flex: 1,
+          maxWidth: '90%',
+          maxHeight: '90%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {/* Image par-dessus */}
+        <div style={{ position: 'relative', width: '150%', height: '100%' }}>
+          <img
+            src="/main/fondpapier.png"
+            alt="Image par dessus"
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+            }}
+          />
+        </div>
+      </div>
+
+      {/* SVG du globe */}
+      <div
+        className="absolute right-0"
+        style={{
+          right: '-41%', // Ajustement pour une position plus à droite sur de plus petits écrans
+          top: '50%', // Centrage vertical
+          transform: 'translateY(-45%)', // Centrage vertical
+          width: '90%', // Ajustement de la largeur pour la rendre responsive
+        }}
+      >
+        <img src="/main/globe.svg" alt="Globe" style={{ width: '100%' }} />
+      </div>
+
+      <button
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full shadow-md"
+        style={{
+          position: 'absolute',
+          bottom: '20%',
+          left: '33%',
+          width: '10%',
+        }}
+        onClick={test}
+      >
+        <p>Démarrer l&apos;exploration</p>
+      </button>
+    </div>
+  );
+};
+
+export default App;
